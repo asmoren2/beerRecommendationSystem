c16c5abc95c1207817d1eaa1d62052fe13d40bc7
Adolfo Moreno <asmoren2@uic.edu>
2017-02-26 15:13:38 -0600

    Added the cluster model so we can group people

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-
-from .models import Beer, Review
+from .models import Beer, Review, Cluster
 
 # Register your models here.
 
@@ -11,5 +10,10 @@ class ReviewAdmin(admin.ModelAdmin):
     list_filter = ['pub_date', 'user_name']
     search_fields = ['comment']
 
+class ClusterAdmin(admin.ModelAdmin):
+    model = Cluster
+    list_display = ['name', 'get_members']
+
 admin.site.register(Beer)
 admin.site.register(Review, ReviewAdmin)
+admin.site.register(Cluster, ClusterAdmin)
